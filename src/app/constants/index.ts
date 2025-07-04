dbdf879be37485b3b680c1aa8efe3498b1efb6b8
Top.theThaiGuy <52368134+kanchanok007@users.noreply.github.com>
2025-07-05 01:29:23 +0700

    Test merge main branch

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,38 +1,24 @@
 interface StoreObj {
 id: string,
-lat: string,
-long:string,
+lat?: string,
+long?:string,
 name: string,
+address?: string,
+phone?: string,
 }
 export const MockupLocation: StoreObj[] = [
   {
     id: "1",
-    lat: "13.7563",
-    long: "100.5018",
-    name: "Bangkok Store",
+    lat: "13.826678422033615",
+    long: "100.57500300673289",
+    name: "บริษัท ที.ซี.ฟาร์มา-เคม จำกัด ",
+    address: "488 ซอยเฉลิมสุข ถนนพหลโยธิน แขวงจันทรเกษม เขตจตุจักร กรุงเทพมหานคร 10900",
+    phone: "02-939-0431",
   },
   {
     id: "2",
-    lat: "18.7883",
-    long: "98.9853",
-    name: "Chiang Mai Outlet",
-  },
-  {
-    id: "3",
-    lat: "7.8804",
-    long: "98.3923",
-    name: "Phuket Branch",
-  },
-  {
-    id: "4",
-    lat: "15.1184",
-    long: "104.9006",
-    name: "Ubon Store",
-  },
-  {
-    id: "5",
-    lat: "16.4419",
-    long: "102.8350",
-    name: "Khon Kaen Mall",
+    name: "ร้านขายยาเภสัชกรไทย",
+    lat: "13.826678422033615",
+    long: "100.57500300673289",
   },
 ];
