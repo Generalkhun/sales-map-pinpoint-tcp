242aafdcdf7015027e8ae9a3b61180ab2e1b9207
suttikhun sihsobhon <khun2551@hotmail.com>
2025-07-04 23:55:03 +0700

    Add search location

---
+++ src/app/constants/index.ts
@@ -0,0 +1,38 @@
+interface StoreObj {
+id: string,
+lat: string,
+long:string,
+name: string,
+}
+export const MockupLocation: StoreObj[] = [
+  {
+    id: "1",
+    lat: "13.7563",
+    long: "100.5018",
+    name: "Bangkok Store",
+  },
+  {
+    id: "2",
+    lat: "18.7883",
+    long: "98.9853",
+    name: "Chiang Mai Outlet",
+  },
+  {
+    id: "3",
+    lat: "7.8804",
+    long: "98.3923",
+    name: "Phuket Branch",
+  },
+  {
+    id: "4",
+    lat: "15.1184",
+    long: "104.9006",
+    name: "Ubon Store",
+  },
+  {
+    id: "5",
+    lat: "16.4419",
+    long: "102.8350",
+    name: "Khon Kaen Mall",
+  },
+];
